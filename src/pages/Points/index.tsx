37823d84638ec0dda301b79280f6b9215185d428
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-05 23:32:12 -0300

    Adding navigate to detail in marker and the styles of marker image

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Feather as Icon } from '@expo/vector-icons'
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
@@ -11,6 +11,10 @@ const Points = () => {
 
     function handleNavigateToBack(){
         navigation.goBack();
+    }
+
+    function handleNavigateToDetail(){
+        navigation.navigate('Detail');
     }
 
     return (
@@ -34,11 +38,18 @@ const Points = () => {
                         }}
                     >
                         <Marker
+                            style={styles.mapMarker}
+                            onPress={handleNavigateToDetail}
                             coordinate={{
                                 latitude: -27.2092052,
                                 longitude: -49.6401092,
                             }}
-                        />
+                        >
+                            <View style={styles.mapMarkerContainer}>
+                                <Image style={styles.mapMarkerImage} source={{ uri: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'}} />
+                                <Text style={styles.mapMarkerTitle}>Mercado</Text>
+                            </View>
+                        </Marker>
                     </MapView>
                 </View>
             </View>
